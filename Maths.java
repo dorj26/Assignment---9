93c8278bd3d8883d5db9852722f9b11135c04977
dorj26 <dorj2693@gmail.com>
2016-08-24 11:09:38 +0530

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,13 @@
+
+public class Maths {
+	
+	public double squareroot(double num)
+	{
+		return Math.sqrt(num);
+	}
+	
+	public double cuberoot(double num)
+	{
+		return Math.cbrt(num);
+	}
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Maths m1 = new Maths();
+		
+		System.out.println(m1.squareroot(25));
+		System.out.println(m1.cuberoot(67));
+	    
+
+	}
+
+}
